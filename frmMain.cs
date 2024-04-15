be38df06987d115dbf4e972036d5864b2d002e8d
Est - IDS FELIZ PEREZ, SELINE <1122831@est.intec.edu.do>
2024-04-14 20:17:47 -0400

    Arreglado

---
+++ frmMain.cs
@@ -0,0 +1,117 @@
+﻿using MiColmado.View2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace MiColmado
+{
+    public partial class frmMain: Sample
+    {
+        static frmMain _obj;
+        public static frmMain Instance
+        {
+            get
+            {
+                if (_obj == null)
+                {
+                    _obj = new frmMain();
+                }
+                return _obj;
+            }
+        }
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        //metodo para añadir las pestañas cuando selecciones home,categoria etc
+        public void AddControls(Form F)
+        {
+            this.centerPanel.Controls.Clear();
+            F.Dock = DockStyle.Fill;
+            F.TopLevel = false;
+            centerPanel.Controls.Add(F);
+            F.Show();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            _obj = this;
+            //para que aparezca el nombre de usuario en la barra lateral
+            lbusuario.Text = MainClass.USER;
+            // Establecer la imagen del perfil
+            pictureBox1.Image = MainClass.IMG;
+        }
+        
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //para que se abra el nuevo formulario de frmHome
+            AddControls(new frmHome());
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //btnEmpleados
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //aqui se abre el formulario user View(el que tiene el datagridview)
+            AddControls(new View.frmUserView()); 
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnCategoria_Click(object sender, EventArgs e)//View2 utilizar
+        {
+            AddControls(new frmCategoryView());
+        }
+
+        private void btnProductos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //para que cuando se cierre el formulario Main aparezca el frmLogin
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            frmLogin frml = new frmLogin();
+            frml.Show();
+        }
+
+        private void btnProveedores_Click(object sender, EventArgs e)
+        {
+            AddControls(new frmSupplierView());
+
+        }
+    }
+
+    //internal class frmCategoryView: Form
+    //{
+    //}
+}
---
+++ frmMain.designer.cs
@@ -0,0 +1,251 @@
+﻿namespace MiColmado
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.Panel = new System.Windows.Forms.Panel();
+            this.btnEmpleados = new System.Windows.Forms.Button();
+            this.btnProveedores = new System.Windows.Forms.Button();
+            this.btnCompras = new System.Windows.Forms.Button();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.btnCategoria = new System.Windows.Forms.Button();
+            this.lbusuario = new System.Windows.Forms.Label();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.centerPanel = new System.Windows.Forms.Panel();
+            this.Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel
+            // 
+            this.Panel.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.Panel.Controls.Add(this.btnEmpleados);
+            this.Panel.Controls.Add(this.btnProveedores);
+            this.Panel.Controls.Add(this.btnCompras);
+            this.Panel.Controls.Add(this.btnProductos);
+            this.Panel.Controls.Add(this.btnCategoria);
+            this.Panel.Controls.Add(this.lbusuario);
+            this.Panel.Controls.Add(this.btnHome);
+            this.Panel.Controls.Add(this.pictureBox1);
+            this.Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Panel.Location = new System.Drawing.Point(0, 0);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(232, 584);
+            this.Panel.TabIndex = 0;
+            this.Panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnEmpleados
+            // 
+            this.btnEmpleados.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnEmpleados.FlatAppearance.BorderColor = System.Drawing.Color.Snow;
+            this.btnEmpleados.FlatAppearance.BorderSize = 0;
+            this.btnEmpleados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmpleados.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmpleados.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnEmpleados.Image = global::MiColmado.Properties.Resources.empleadosIcon;
+            this.btnEmpleados.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEmpleados.Location = new System.Drawing.Point(10, 477);
+            this.btnEmpleados.Name = "btnEmpleados";
+            this.btnEmpleados.Size = new System.Drawing.Size(197, 40);
+            this.btnEmpleados.TabIndex = 7;
+            this.btnEmpleados.Text = "Empleados";
+            this.btnEmpleados.UseVisualStyleBackColor = true;
+            this.btnEmpleados.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btnProveedores
+            // 
+            this.btnProveedores.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnProveedores.FlatAppearance.BorderColor = System.Drawing.Color.Snow;
+            this.btnProveedores.FlatAppearance.BorderSize = 0;
+            this.btnProveedores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProveedores.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProveedores.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnProveedores.Image = global::MiColmado.Properties.Resources.proveedoresIcon;
+            this.btnProveedores.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProveedores.Location = new System.Drawing.Point(10, 420);
+            this.btnProveedores.Name = "btnProveedores";
+            this.btnProveedores.Size = new System.Drawing.Size(197, 40);
+            this.btnProveedores.TabIndex = 6;
+            this.btnProveedores.Text = "Proveedores";
+            this.btnProveedores.UseVisualStyleBackColor = true;
+            this.btnProveedores.Click += new System.EventHandler(this.btnProveedores_Click);
+            // 
+            // btnCompras
+            // 
+            this.btnCompras.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCompras.FlatAppearance.BorderColor = System.Drawing.Color.Snow;
+            this.btnCompras.FlatAppearance.BorderSize = 0;
+            this.btnCompras.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCompras.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompras.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCompras.Image = global::MiColmado.Properties.Resources.comprasIcon;
+            this.btnCompras.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCompras.Location = new System.Drawing.Point(12, 374);
+            this.btnCompras.Name = "btnCompras";
+            this.btnCompras.Size = new System.Drawing.Size(197, 40);
+            this.btnCompras.TabIndex = 5;
+            this.btnCompras.Text = "Compras";
+            this.btnCompras.UseVisualStyleBackColor = true;
+            this.btnCompras.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnProductos.FlatAppearance.BorderColor = System.Drawing.Color.Snow;
+            this.btnProductos.FlatAppearance.BorderSize = 0;
+            this.btnProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProductos.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProductos.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnProductos.Image = global::MiColmado.Properties.Resources.productosIcon;
+            this.btnProductos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProductos.Location = new System.Drawing.Point(10, 319);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(197, 40);
+            this.btnProductos.TabIndex = 4;
+            this.btnProductos.Text = "Productos";
+            this.btnProductos.UseVisualStyleBackColor = true;
+            this.btnProductos.Click += new System.EventHandler(this.btnProductos_Click);
+            // 
+            // btnCategoria
+            // 
+            this.btnCategoria.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCategoria.FlatAppearance.BorderColor = System.Drawing.Color.Snow;
+            this.btnCategoria.FlatAppearance.BorderSize = 0;
+            this.btnCategoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCategoria.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategoria.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCategoria.Image = global::MiColmado.Properties.Resources.categoriaIcon;
+            this.btnCategoria.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCategoria.Location = new System.Drawing.Point(10, 253);
+            this.btnCategoria.Name = "btnCategoria";
+            this.btnCategoria.Size = new System.Drawing.Size(197, 40);
+            this.btnCategoria.TabIndex = 3;
+            this.btnCategoria.Text = "Categoria";
+            this.btnCategoria.UseVisualStyleBackColor = true;
+            this.btnCategoria.Click += new System.EventHandler(this.btnCategoria_Click);
+            // 
+            // lbusuario
+            // 
+            this.lbusuario.AutoSize = true;
+            this.lbusuario.BackColor = System.Drawing.Color.Transparent;
+            this.lbusuario.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbusuario.Location = new System.Drawing.Point(48, 130);
+            this.lbusuario.Name = "lbusuario";
+            this.lbusuario.Size = new System.Drawing.Size(125, 17);
+            this.lbusuario.TabIndex = 2;
+            this.lbusuario.Text = "Nombre de Usuario";
+            this.lbusuario.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnHome.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnHome.Image = global::MiColmado.Properties.Resources.homa__1_;
+            this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.Location = new System.Drawing.Point(10, 186);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(197, 40);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(51, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.centerPanel);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(232, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(749, 584);
+            this.panel2.TabIndex = 1;
+            // 
+            // centerPanel
+            // 
+            this.centerPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.centerPanel.Location = new System.Drawing.Point(7, 12);
+            this.centerPanel.Name = "centerPanel";
+            this.centerPanel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.centerPanel.Size = new System.Drawing.Size(730, 560);
+            this.centerPanel.TabIndex = 0;
+            this.centerPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 584);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Panel.ResumeLayout(false);
+            this.Panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel centerPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Label lbusuario;
+        private System.Windows.Forms.Button btnCompras;
+        private System.Windows.Forms.Button btnProductos;
+        private System.Windows.Forms.Button btnCategoria;
+        private System.Windows.Forms.Button btnEmpleados;
+        private System.Windows.Forms.Button btnProveedores;
+    }
+}
